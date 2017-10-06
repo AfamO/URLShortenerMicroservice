de6d5c9ab299ea4fd6878ac1083ae9a8ecbf4025
System <none>
2017-10-06 15:09:58 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -37,9 +37,49 @@ app.use(express.static('public'));
 //{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
 // http://expressjs.com/en/starter/basic-routing.html
 
-app.get("/[0-9]", function (request, response) {
-  response.send("Thank you!");
-  //response.sendFile(__dirname + '/views/index.html');
+app.get("/[0-9]*", function (request, response) {
+  var counter=request.url.replace("https://rich-alto.glitch.me/","");
+  // Use connect method to connect to the Server 
+  MongoClient.connect(dbUrl, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to my', dbUrl);
+    var collection=db.collection('url-shortener');
+    if(collection!=null){
+      var query={_id:counter};
+      counter=+counter;
+      collection.findOne(query,function(err,data){
+        if (err) throw err;
+        console.log(data);
+        //Is the original url already existing in DB?
+        if(data!=null)
+          {
+             if(data.original_url==originalUrl)
+            {
+                console.log(originalUrl +" already exists in DB.");
+                db.close();
+                response.send(data);
+            }
+          }
+        else
+          {
+           response.send("Thank you!");
+          }
+      });//End findOne callback
+    }
+      else{
+        console.log('Could not find collection url-shortener');
+    }
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});//Close outer DB connection
+
+  
 });
 app.get("/new/*", function (request, response) {
   var parsedUrl=url.parse(request.url, true);
